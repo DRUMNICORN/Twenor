390fea4864e5695db95474f14eea9933852b6972
roggen <robin@seerig.de>
2023-01-20 13:20:53 +0100

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,27 @@
  *  - Content
  */
 
-import React from 'react';
+import React from "react";
 
-import Titlebar from './components/Titlebar';
-import Sidebar from './components/Sidebar';
-import Content from './components/Content';
+import Titlebar from "./components/Titlebar";
+import Sidebar from "./components/Sidebar";
+import Content from "./components/Content";
 
 // In this file we can import the tauri api
 import { appWindow } from "@tauri-apps/api/window";
+import Explorer from "./components/Explorer";
+import Logo from "./components/design/Logo";
+
+import "./App.scss";
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Titlebar />
-      <Sidebar />
+      <Sidebar>
+        <Logo />
+        <Explorer />
+      </Sidebar>
       <Content />
     </div>
   );
